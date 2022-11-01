575c50c8e4dbdab60fae7d5ebc2aaf9db1312460
Ingrid Carlson <ingridclaire@gmail.com>
2022-11-01 13:26:13 -0400

    removes unneeded files and sets up react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Chat from './components/Chat';
+import Login from './components/Login';
+import Header from './components/Header';
+import { Container } from './styled/styled';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Container>
+          <Header />
+          <Routes>
+            <Route path="/room" element={<Chat />}/>
+             
+            <Route exact path="/" element={<Login />}/>
+          </Routes>
+        </Container>
+      </Router>
     </div>
   );
 }
